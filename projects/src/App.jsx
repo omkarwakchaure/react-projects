694f741d31e5a13eccfd04645f921706eecbc5b4
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2025-01-03 17:25:00 +0530

    ImageSlider

--- projects/src/App.jsx
+++ projects/src/App.jsx
@@ -1,17 +1,21 @@
 import Accordian from "./components/accordian/Accordian";
+import ImageSlider from "./components/image-slider/ImageSlider";
 import RandomColor from "./components/random-color/RandomColor";
 import StarRating from "./components/star-rating/StarRating";
 function App() {
   return (
     <>
-    {/* Accordian Component */}
+      {/* Accordian Component */}
       {/* <Accordian/> */}
 
-    {/* RandomColor Component */}
+      {/* RandomColor Component */}
       {/* <RandomColor/> */}
 
-    {/* Star Rating */}
-    <StarRating/>
+      {/* Star Rating Components */}
+      {/* <StarRating/> */}
+
+      {/* Image Slider Components */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={"10"} />
     </>
   );
 }
